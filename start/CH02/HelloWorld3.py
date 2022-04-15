67620cb705eb8e78a8a9410cf45cc45c9c8dbfcc
Jinsunjo <Srilofung.Jason@student.greenriver.edu>
2022-04-15 14:58:34 -0700

    Different ways to print messages

--- start/CH02/HelloWorld3.py
+++ start/CH02/HelloWorld3.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
 # A more complex "Hello World" script in python with Inputs
 # Created 
+
+#Ask for user input
+user_name = input("What is your name? ")
+
+print("Hello {0}".format(user_name))
+print(f"Hello {user_name}")
+print("Hello " + user_name)
+print("Hello " , user_name )
+message = "Hello " + user_name
+print(message)
