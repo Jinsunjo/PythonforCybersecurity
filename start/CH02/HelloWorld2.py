c3631b0d370d3705bbe554b9c4899a932a27554f
Jinsunjo <Srilofung.Jason@student.greenriver.edu>
2022-04-15 14:55:05 -0700

    My first input

--- start/CH02/HelloWorld2.py
+++ start/CH02/HelloWorld2.py
@@ -5,3 +5,12 @@
 # Suggestion, build out 1 line at a time
 # Once multiple print statemetns exist, put a breakpoint at first print line
 # Then walk through as an example of "debugging"
+
+#Ask for the user for their name
+user_name = input("What is your name? ")
+
+#Print hello and their name
+
+print("Hello {0}".format(user_name))
+
+
