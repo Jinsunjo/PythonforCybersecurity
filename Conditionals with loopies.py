52c2355950de4f0a5c624ce353bc56b8c8a6d71c
Jinsunjo <Srilofung.Jason@student.greenriver.edu>
2022-05-01 19:32:19 -0700

    updating and making it a function

--- Conditionals with loopies.py
+++ Conditionals with loopies.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 # A Conditional script in python
-# Created by Jason Srilofung 4/24/2022
+# Created by Jason Srilofung 5/1/2022
 #The three goals is ask the user how is their day and takes the imput and print it out
 #If the answer is y print out yes it is
+# Update make the loop a def function to be recalled
 
 #takes input of the user's name
 
@@ -15,16 +16,20 @@ print("greetings {0}".format(userName))
 userDay = input("Is today a good day? (y/n) ")
 
 #The start of the if statement
-
-if userDay == "y":
-    print("Yes it is {0}".format(userName))
+def conditionals():
+    if userDay == "y":
+        print("Yes it is {0}".format(userName))
 
     for x in range(10):
         print("Yes it is {0}".format(userName))
 
-else:
-    print("Hope you day gets better {0}".format(userName))
+    else:
+        print("Hope you day gets better {0}".format(userName))
 
 #print yes it is 10 times
+
+#recall the fucntion
+
+conditionals()
 
 
