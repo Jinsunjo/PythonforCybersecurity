0d2b4aebc856783d098adc1afbe59e3df306e142
Jinsunjo <Srilofung.Jason@student.greenriver.edu>
2022-05-10 21:58:07 -0700

    My program for even or odd

---
+++ Even or odd.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# A Conditional script in python
+# Created by Jason Srilofung 5/10/2022
+#Write a Script that reads the imput of the user and tell if it is even or odd
+#prints the answers to the user
+
+number = int (input ("Enter a number to test whether it's odd or even: "))
+
+if (number % 2) == 0:
+    print("The number is even")
+    
+
+else:
+    print("The number is odd")
+
